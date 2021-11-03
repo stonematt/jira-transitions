785af7c6ff348cfc87253ca965c060d2317127e4
Matt Stone <mstone@thebrightlink.com>
2021-11-03 16:35:09 +0000

    Merged in Add-smart-bins-from-lifecycle-phase (pull request #4)
    Add-smart-bins-from-lifecycle-phase
    
    * get basics working using lifecylcle variable
    * separate dataframe production from output
    * Record filter and lifecycle in dataframes
    * Move to function to get data and print
    * Add support for jobs
    * update estimating start date
    * Update to include NaN in counts for aging
    * Add pending approval to lifecycles

--- jira_transistions.py
+++ jira_transistions.py
@@ -6,11 +6,10 @@ import json
 import pandas as pd
 from datetime import datetime
 
-# from pprint import pprint
-
 
 # this includes an individuals api key.  use it carefully it.
 token = json.load(open("me.json"))
+datadir = "./data/"
 
 
 def _get(url, params={}):
@@ -91,14 +90,6 @@ def get_issue(issueid):
     return issue
 
 
-def get_issue_transistions(issueid):
-    issueid = issueid
-    url = "issue/" + issueid + "/transitions"
-
-    issue_transitions = _get(url)
-    return issue_transitions
-
-
 def get_issue_changelog(issueid):
     """Return issue dictionary of issue change log """
 
@@ -164,12 +155,12 @@ def get_status_changes_summary(issue_log, status_list):
         status_changes[status + "_name"] = status
         status_changes[status + "_first"] = ""
         status_changes[status + "_last"] = ""
-        status_changes[status + "_count"] = 0
+        status_changes[status + "_transitions"] = 0
 
     for v in issue_log["values"]:
         for i in v["items"]:
             if i["field"] == "status" and i["toString"] in status_list:
-                status_changes[i["toString"] + "_count"] += 1
+                status_changes[i["toString"] + "_transitions"] += 1
                 # set the first date
                 if status_changes[i["toString"] + "_first"] == "":
                     status_changes[i["toString"] + "_first"] = v["created"]
@@ -190,22 +181,49 @@ def get_status_changes_summary(issue_log, status_list):
     return status_changes
 
 
-def issues_to_pandas(status_changes):
+def _generate_aging_names(lifecycle_phase):
+    aging_start = lifecycle_phase["first_status"] + "_last"
+    aging_name = lifecycle_phase["phase_code"] + "_age"
+    aging_bins = aging_name + "_bins"
+
+    return aging_start, aging_name, aging_bins
+
+
+def issues_to_pandas(status_changes, lifecycle_phase):
+    """Converts dictionary of status changes to pandas dataframe
+    that includes aging and estimating bins
+
+    Args:
+        status_changes (dict): jira change log of status changes
+        lifecycle_phase (dict): configuration of lifecycle. need keys for
+                                 "first_status" and "phase_code"
+
+    Returns:
+        dataframe:  dataframe with age from phase start,
+                    ammended with jira filter and lifecycle phase
     """
-    return dataframe of age of status
-    issue_log - dictionary of jira change log
-    status_list - list of statuses to extract"""
+    # aging_start = lifecycle_phase["first_status"] + "_last"
+    # aging_name = lifecycle_phase["phase_code"] + "_age"
+    # aging_bins = aging_name + "_bins"
+
+    aging_start, aging_name, aging_bins = _generate_aging_names(lifecycle_phase)
+
     sc = pd.DataFrame(status_changes)
-    sc["In Backlog_last"] = pd.to_datetime(sc["In Backlog_last"], utc=True)
+    sc[aging_start] = pd.to_datetime(sc[aging_start], utc=True)
+    sc["created"] = pd.to_datetime(sc["created"], utc=True)
     # todo: make other dates be dates
 
-    sc["approved_age"] = (pd.Timestamp.now(tz="UTC") - sc["In Backlog_last"]).dt.days
+    sc[aging_name] = (
+        (pd.Timestamp.now(tz="UTC") - sc[aging_start]).where(
+            pd.notna(sc[aging_start]), pd.Timestamp.now(tz="UTC") - sc["created"]
+        )
+    ).dt.days
     # todo: calculate days remaining
 
     # set aging bins
     bins = [0, 7, 14, 30, 60, 90, 120, 1000]
     lables = ["07d", "14d", "30d", "60d", "90d", "q120+", "very old"]
-    sc["approval_age_bins"] = pd.cut(sc["approved_age"], bins=bins, labels=lables)
+    sc[aging_bins] = pd.cut(sc[aging_name], bins=bins, labels=lables)
 
     # set estimate bins
     estbins = [0, 500, 1000, 2000, 5000, 10000, 20000, 1000000]
@@ -229,11 +247,11 @@ def category_distribution(status_changes, column):
         dataframe: aggregation grouped by named column
     """
 
-    print(f"\nTotaling: ${status_changes['client_estimate'].sum()}")
+    # print(f"\nTotaling: ${status_changes['client_estimate'].sum()}")
 
     return (
         status_changes.groupby(column)["client_estimate"]
-        .agg(Count="count", SumVal="sum", AvgVal="mean")
+        .agg(Count="size", SumVal="sum", AvgVal="mean")
         .round(2)
     )
 
@@ -249,23 +267,25 @@ def export_json(dict, file):
         json.dump(dict, fp, sort_keys=True, indent=2)
 
 
-def get_working_issues(status_list, source="jira"):
-    """use local or jira as issue source
+def get_working_issues(status_list, jira_filter, source="jira"):
+    """get issues to process.  supports call to jira or local cached version
 
-    Arguments:
-        status_list -- list of jira statuses to collect transistion data for
+    Args:
+        status_list (list): list of jira statuses to collect transistion data for
+        jira_filter (string): name of jira filter to retrieve
+        source (str, optional): "jira" to go to jira or "local" to open local file. Defaults to "jira".
 
-        source -- "jira" to go to jira or "local" to open local file
+    Raises:
+        nofile: if local file of cached issues not found
 
     Returns:
-        dict of issues w/ status transition information
+        dict: issues w/ status transition information
     """
 
     w_issues_file = "./examples/" + status_list["filename"]
     if source == "jira":
         w_jira_issues = get_issues_from_filter(jira_filter)
-        w_issues = get_transistions_for_issues(w_jira_issues, sold_statuses["statuses"])
-        # pprint(sold_issues)
+        w_issues = get_transistions_for_issues(w_jira_issues, status_list["statuses"])
         export_json(w_issues, w_issues_file)
 
     if source == "local":
@@ -278,51 +298,140 @@ def get_working_issues(status_list, source="jira"):
     return w_issues
 
 
-# initally, we'll evaluate aging on "sold" status. but a similar evalutaion of
-# pending status is likely worthwhile.
-sold_statuses = {
-    "filename": "sold_statuses.json",
+def _amend_df(issues_df, phase_code, jira_filter):
+    """Data pipeline step add today's date and meta data prior to saving
+
+    Args:
+        issues_df (dataframe): dataframe to ammend
+        phase_code ([type]): [description]
+        jira_filter ([type]): [description]
+
+    Return:
+        dataframe
+    """
+    issues_df["Date"] = pd.Timestamp.now().date()
+    issues_df["phase_code"] = phase_code
+    issues_df["jira_filter"] = jira_filter
+
+    return issues_df
+
+
+def get_snapshot(lifecycle, jira_filter):
+    """Get current data snapshot from jira for a lifecycle phase from a jira filter
+    This is the content to send to metabase
+
+    Args:
+        lifecycle (dict): lifecycle phase in the jira sales pipeline
+        jira_filter (string): name of a saved jira filter
+
+    Returns:
+        dict: collection of 3 dataframes with snapshot anylysis:
+            df sshot_description: description of the snapshot
+            df aging_dist: aging distrbution
+            df client_estimate_dist: distribution of client estimates
+    """
+
+    aging_start, aging_name, aging_bins = _generate_aging_names(lifecycle)
+
+    print(f"Fetching data from {jira_filter}")
+    # use "local" or "jira" to indicate whether to actually hit the jira api.
+    snap_shot = get_working_issues(lifecycle, jira_filter, "jira")
+    snap_shot_dfs = {}
+
+    # make relevant dataframes
+    sshot = issues_to_pandas(snap_shot, lifecycle)
+
+    snap_shot_dfs["sshot_description"] = sshot.describe(
+        percentiles=[0.25, 0.5, 0.8, 0.9]
+    ).round(2)
+
+    snap_shot_dfs["aging_dist"] = category_distribution(sshot, aging_bins)
+
+    snap_shot_dfs["client_estimate_dist"] = category_distribution(
+        sshot, "client_estimate_bins"
+    )
+
+    # add columns for todays date, phase, and jira filter for reporting
+    # snap_shot_dfs = [sshot_description, aging_dist, client_estimate_dist]
+
+    for key, df in snap_shot_dfs.items():
+        snap_shot_dfs[key] = _amend_df(df, lifecycle["phase_code"], jira_filter)
+
+    # return sshot_description, aging_dist, client_estimate_dist
+    return snap_shot_dfs
+
+
+def print_snapshot(sshot_description, aging_dist, client_estimate_dist):
+
+    # print results
+    print("==== simple reiview of issues ====")
+    print(sshot_description)
+
+    print("\nHistogram of Aging")
+    print(aging_dist)
+
+    print("\nHistogram of client_estimate")
+    print(client_estimate_dist)
+
+
+# create running panda parquet files for
+# . summary
+# . aging
+# . estimate bins
+#
+# for each job, script should support
+# init (load historical data)
+# load working data (filter to job, return pd)
+# refresh data (open saved files, get new data from jira, ammend to saved file - 1/day most recent)
+
+"""Defie list of estimating life cycle phases to analyze.  Each may have a list
+of jira filters to serve as datasets in the life cycle to evalutate"""
+
+approved_waiting = {
+    "filename": "approved_waiting.json",
     "statuses": ["In Backlog", "Scheduled"],
     "first_status": "In Backlog",
     "phase_code": "approved_waiting",
+    "jira_filters": [
+        "backlog_approved_waiting",
+        "backlog_approved_waiting_solutions_only",
+    ],
 }
 
-pending_statuses = {
+pending_approval = {
     "filename": "pending_statuses.json",
-    "statuses": ["Sent to Client", "Response Review"],
+    "statuses": ["Response Review", "Pending Client Input", "Sent to Client"],
     "first_status": "Sent to Client",
     "phase_code": "pending_approval",
+    "jira_filters": ["backlog_sent_to_client"],
 }
 
+estimating = {
+    "filename": "estimating.json",
+    "statuses": ["Planning", "Needs Estimate", "Prep Client SOW"],
+    "first_status": "Planning",
+    "phase_code": "estimating",
+    "jira_filters": ["backlog_estimating_all"],
+}
+
+"""List of jobs of life cycles to iterate over"""
+# lifecycles = [approved_waiting, pending_approval]
+lifecycles = [estimating, pending_approval, approved_waiting]
+# lifecycles = [estimating]
+
 # %%
-# jira_filter = "backlog_approved_waiting"
-jira_filter = "backlog_approved_waiting_solutions_only"
 
-print(f"Fetching data from {jira_filter}")
-# use "local" or "jira" to indicate whether to actually hit the jira api.
-# todo: refactor get working issues to take jira_filter as arg
-sold_issues = get_working_issues(sold_statuses, "jira")
+for lc in lifecycles:
+    for jfilter in lc["jira_filters"]:
+        new_snapshot = get_snapshot(lc, jfilter)
+        # todo: save to history here. (send to metabase?)
+        print_snapshot(
+            new_snapshot["sshot_description"],
+            new_snapshot["aging_dist"],
+            new_snapshot["client_estimate_dist"],
+        )
 
-sc = issues_to_pandas(sold_issues)
-print("==== simple reiview of issues ====")
-sc_description = sc.describe(percentiles=[0.25, 0.5, 0.8, 0.9]).round(2)
-sc_description["Date"] = pd.Timestamp.now().date()
-print(sc_description)
-# print(sc.describe(percentiles=[0.25, 0.5, 0.8, 0.9]).round(2))
-
-# experiment with prepping to save historical timeseries
-approval_age_history = category_distribution(sc, "approval_age_bins")
-approval_age_history["Date"] = pd.Timestamp.now().date()
-
-print("\nHistogram of days since approval")
-print(approval_age_history)
-
-client_estimate_dist = category_distribution(sc, "client_estimate_bins")
-client_estimate_dist["Date"] = pd.Timestamp.now().date()
-
-print("\nHistogram of client_estimate")
-print(client_estimate_dist)
-# print(category_distribution(sc, "client_estimate_bins"))
-
-# todo: future program: run script w/ filtername and phase as args
 # %%
+
+# if __name__ == "__main__":
+# main(parse_args())
